9c53d8863f826b72a5f20ebb8148645b038e606a
Fabian Stegemann <fabian@stegemann.one>
2022-05-29 14:52:59 +0200

    add footprint to bat

--- src/footprints/bat.js
+++ src/footprints/bat.js
@@ -10,6 +10,11 @@ module.exports = {
     
     (module lib:bat (layer F.Cu) (tstamp 5BF2CC94)
         ${p.at /* parametric position */}
+	
+	${'' /* footprint reference */}
+        (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
+        (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
+	
 	  (pad 1 thru_hole circle (at 0 -0.75 0) (size 1 1) (drill 0.7) (layers *.Cu *.SilkS *.Mask) ${p.net.pos.str}) 
 	  (pad 2 thru_hole circle (at 0 0.75 0) (size 1 1) (drill 0.7) (layers *.Cu *.SilkS *.Mask) ${p.net.neg.str})
      )
